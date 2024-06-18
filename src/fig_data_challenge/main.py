7055fd623a9c9aa6f14dd2847c4f7994478179d6
Connor Segneri <connorsegneri@gmail.com>
2024-06-18 08:18:24 -0700

    feat: correct mispellings in column "Store"

--- src/fig_data_challenge/main.py
+++ src/fig_data_challenge/main.py
@@ -7,23 +7,30 @@ def main() -> None:
     df_menu_items = pd.read_excel(
         "data/restaurant_data.xlsx", sheet_name="Restaurant Menu Items"
     )
-    # df_reference_categories = pd.read_excel(
-    #     "data/restaurant_data.xlsx", sheet_name="Reference categories"
-    # )
+    df_reference_categories = pd.read_excel(
+        "data/restaurant_data.xlsx", sheet_name="Reference categories"
+    )
 
-    # drop rows where "Product Name" is blank or null
+    # drop rows where blank or null
     print(df_menu_items.shape)
     df_menu_items["Product Name"] = df_menu_items["Product Name"].replace("", np.nan)
-    df_menu_items = df_menu_items.dropna(subset=["Product Name"])
-    print(df_menu_items.shape)
-
-    # drop rows where "Ingredients on Product Page" is blank or null
-    print(df_menu_items.shape)
     df_menu_items["Ingredients on Product Page"] = df_menu_items[
         "Ingredients on Product Page"
     ].replace("", np.nan)
-    df_menu_items = df_menu_items.dropna(subset=["Ingredients on Product Page"])
+    df_menu_items["Store"] = df_menu_items["Store"].replace("", np.nan)
+    df_menu_items = df_menu_items.dropna(
+        subset=["Product Name", "Ingredients on Product Page", "Store"]
+    )
     print(df_menu_items.shape)
+
+    # check the store names for any mispellings
+    print(np.sort(df_menu_items["Store"].unique()))
+    # correct the mispellings
+    df_menu_items.loc[df_menu_items["Store"] == "MacDonald's", "Store"] = "McDonald’s"
+    print(np.sort(df_menu_items["Store"].unique()))
+
+    # check the category names for any mispellings
+    print(np.sort(df_reference_categories["Fig Category 1"].unique()))
 
 
 if __name__ == "__main__":
